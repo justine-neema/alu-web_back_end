5c8dbc858ff5cc2c8bc062b2b4437e970137d6b3
justine-neema <j.neema@alustudent.com>
2026-05-12 20:51:25 +0200

    first commit

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""this module contains basic type-annotated function for addition"""
+
 def add (a: float, b:float) -> float:
     """Return the sum of two float numbers."""
     return a + b
